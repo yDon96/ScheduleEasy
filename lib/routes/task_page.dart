39e321df76af2348eafeb7ad979b93c98a330f68
Youssef Donadeo <you.donadeo@icloud.com>
2022-08-20 17:59:40 +0200

    Design Task page

--- lib/routes/task_page.dart
+++ lib/routes/task_page.dart
@@ -34,39 +34,152 @@ class _TaskPageState extends State<TaskPage> {
     return Scaffold(
         appBar: TopBar(username: 'John Doe', imageUrl: "https://static.wikia.nocookie.net/zelda/images/d/d4/Link_ssbu.jpg/revision/latest?cb=20180614072525&path-prefix=it"),
         body:Column(
-
           children:  [
-            const Text("Add new Task:"),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  labelText: 'Title',
-                ),
+            Expanded(
+              flex: 6,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.8),
+                child: ListView(
+                  children:  [
+                    Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children:  const [
+                        TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'New Task Title',
+                            ),
+                            style: TextStyle(fontSize: 32), // <-- SEE HERE
+                          ),
+                        TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Simple Description',
+                            ),
+                            style: TextStyle(fontSize: 16),
+                            maxLines: 5, // <-- SEE HERE
+                            minLines: 1, // <-- SEE HERE// <-- SEE HERE
+                          ),
+                      ],
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey,
+                        ),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: const [
+                                  Padding(
+                                      padding: EdgeInsets.only(left: 3.0),
+                                      child: Text(
+                                        "Status",
+                                        style: TextStyle(fontSize: 18),
+                                      )
+                                  ),
+                                ],
+                              ),
+                              const Text("Not Started")
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: const [
+                                  Icon(Icons.access_time),
+                                  Padding(
+                                      padding: EdgeInsets.only(left: 3.0),
+                                      child: Text(
+                                        "Due Time",
+                                        style: TextStyle(fontSize: 18),
+                                      )
+                                  ),
+                                ],
+                              ),
+                              const Text("18:00"),
+                              const Text("August 10, 2022"),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: const [
+                            Icon(Icons.adf_scanner_outlined),
+                            Padding(
+                                padding: EdgeInsets.only(left: 3.0),
+                                child: Text("Tags")
+                            ),
+                          ],
+                        ),
+                        OutlinedButton.icon(
+                          onPressed: () {
+                            // Respond to button press
+                          },
+                          icon: const Icon(Icons.add, size: 18),
+                          label: const Text("Add"),
+                        )
+                    ]),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: const [
+                            Icon(Icons.task_alt),
+                            Padding(
+                                padding: EdgeInsets.only(left: 3.0),
+                                child: Text("SubTask")
+                            ),
+                          ],
+                        ),
+                        OutlinedButton.icon(
+                          onPressed: () {
+                            // Respond to button press
+                          },
+                          icon: const Icon(Icons.add, size: 18),
+                          label: const Text("Add"),
+                        )
+                      ],
+                    )
+                  ]),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 8.8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                  labelText: 'Description',
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 8.8),
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25.0)),
+                      minimumSize: const Size.fromHeight(100), // NEW
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(Icons.bookmark_border_rounded),
+                        Text(
+                          'Save',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                      ],
+                    )
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 8.8),
-              child: FloatingActionButton.extended(
-                onPressed: () {},
-                icon: const Icon(Icons.bookmark_border_rounded),
-                label: const Text("Save"),
-                elevation: 0,
-              ),
+              )
             ),
           ],
         )
